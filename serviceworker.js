8864487663117f5f68ef9772372dbc6be1ee6e56
Augusto215 <augusto.dataanalysis@gmail.com>
2024-02-18 01:37:07 -0300

    novo21

--- serviceworker.js
+++ serviceworker.js
@@ -1,9 +1,12 @@
 var staticCacheName = "pwa";
  
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/"]);
+self.addEventListener('install', function(event) {
+  self.skipWaiting(); // Ativa o Service Worker mais rápido
+
+  event.waitUntil(
+    self.registration.showNotification('Bem-vindo ao NUBANK PWA', {
+      body: 'Obrigado por instalar nosso app!',
+      icon: 'assets/img/anne-peres-plsF6obTgms-unsplash.jpg'
     })
   );
 });
@@ -15,5 +18,20 @@ self.addEventListener("fetch", function (event) {
     caches.match(event.request).then(function (response) {
       return response || fetch(event.request);
     })
+  );
+});
+
+self.addEventListener('push', function(event) {
+  var options = {
+    body: 'Esta é uma mensagem de notificação push!',
+    icon: 'assets/img/anne-peres-plsF6obTgms-unsplash.jpg',
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: 1
+    }
+  };
+  event.waitUntil(
+    self.registration.showNotification('Push Notificação', options)
   );
 });
